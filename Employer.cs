69ba78ec1704eb3ac50db19dfcc582e95cfbd69d
Farkas <farasz@kkszki.hu>
2024-09-19 09:33:47 +0200

    Employer constructor is ready.

--- Employer.cs
+++ Employer.cs
@@ -6,5 +6,15 @@
         public string Name { get; set; }
         public int Age { get; set; }
         public int Salary { get; set; }
+
+        public Employer(string row)
+        {
+            string[] darabol = row.Split(';');
+
+            Id = int.Parse(darabol[0]);
+            Name = darabol[1];
+            Age = int.Parse(darabol[2]);
+            Salary = int.Parse(darabol[3]);
+        }
     }
 }
