06163934dea16453fd303942d8db691cdfb46596
Farkas <farasz@kkszki.hu>
2024-09-19 09:44:49 +0200

    Text file is used.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Employees
 {
     internal class Program
     {
+        public static List<Employer> employers = new List<Employer>();
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("tulajdonsagok_100sor.txt");
+
+            Employer emp = new Employer(sr.ReadLine());
+            Employer emp2 = new Employer(sr.ReadLine());
+
+            Console.WriteLine(emp.Name);
+            Console.WriteLine(emp2.Name);
         }
     }
 }
